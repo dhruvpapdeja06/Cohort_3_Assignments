1986d39d11be950c0e9406aa803c8a5141107e03
dhruvpapdeja06 <dhruv2024papdeja@gmail.com>
2026-06-15 12:18:55 +0530

    Test2.1 function practise question completed

--- Assignments/JavaScript/Test-Phase/Test2.1/script.js
+++ Assignments/JavaScript/Test-Phase/Test2.1/script.js
@@ -348,3 +348,228 @@ function login(username,password){
 
 login("admin","1234");
 
+/*
+
+🔴 Hard Level (Q15 - Q20)
+These questions require problem-solving and function composition.
+---------------------------------------------------------------------------------
+Question 15 — Factorial Function
+Create:
+factorial(n)
+Example:
+factorial(5)
+Output:
+
+120
+
+Because:
+5×4×3×2×1
+
+Scenario
+Math engine.
+
+*/
+
+// recursion --> stack overflow problem
+function factorial(n){
+    if(n === 1 || n === 0) return 1;
+
+    return n * factorial(n-1);
+}
+
+console.log(factorial(5));
+
+// using for loop , while loop
+
+function fact(n){
+    let result = 1;
+    
+    for(let i=2; i<=n; i++){
+        result *= i;
+    }
+    return result;
+}
+
+console.log(fact(6));
+
+//1. Memoization or Caching --> to solve recursion problem
+// TCO
+
+
+/*
+
+Question 16 — Palindrome Checker
+Create:
+isPalindrome(str)
+Example:
+isPalindrome("madam")
+Output:
+true
+
+Scenario
+Text validation tool.
+
+*/
+
+// same fow while loop --. str.length-1-i
+function isPalindrome(str){
+    for(let left=0,right=str.length-1; left<right; left++,right--){
+        if(str[left] !== str[right]) return "Not Palindrome"
+    }
+    return "Palindrome";
+}
+
+console.log(isPalindrome("madam"));
+
+
+/*
+Question 17 — Password Strength Checker
+Create:
+checkPassword(password)
+Rules:
+Password should contain:
+At least 8 characters
+At least 1 number
+Return:
+"Strong"
+
+or
+"Weak"
+
+Scenario
+Registration page.
+
+
+*/
+/*
+function checkPassword(password){
+    if(password.length >= 8 && /[A-Z]/.test(password) && /[0-9]/.test(password)){
+        console.log("Strong");
+    }else{
+        console.log("Weak");
+    }
+}
+
+console.log(checkPassword("Dhruv78600"));
+
+*/
+
+function checkPassword(password){
+    if(password.length < 8){
+        console.log("Weak Password");
+    }
+
+    let hasCaptial = false;
+    let hasNumber = false
+
+    for(let i=0; i<password.length; i++){
+        let char = password[i];
+
+        if(char >= "A" && char <= "Z"){
+            hasCaptial = true;
+        }
+
+        if(char >= "0" && char <= '9'){
+            hasNumber = true;
+        }
+    }
+
+    if(hasCaptial && hasNumber){
+        console.log("strong Password");
+    }else{
+        console.log("Weak Password");
+    }
+}
+
+checkPassword("Ishan7866");
+
+/*
+
+Question 18 — Function Returning Function
+Create:
+makeMultiplier(multiplier)
+Example:
+letdouble=makeMultiplier(2);
+double(10);
+Output:
+20
+
+Scenario
+Reusable calculation system.
+
+
+*/
+
+function makeMultiplier(multiplier){
+    function mulitply(num){
+        return num * multiplier;
+    }
+    return mulitply;
+}
+
+let double = makeMultiplier(2);
+
+console.log(double(10));
+
+
+/*
+Question 19 — Callback Function
+Create:
+processUser(name,callback)
+Example:
+
+processUser("Ritik",welcomeUser);
+Output:
+Processing User...
+WelcomeRitik
+
+Scenario
+Backend processing.
+
+
+*/
+
+function processUser(name,cb){
+    console.log("processing User ...");
+    cb(name);
+}
+
+function welcomeUser(name){
+    console.log( `Welcome ${name}`);
+}
+
+processUser("Ritik",welcomeUser);
+
+
+/*
+
+Question 20 — Shopping Bill Generator
+Create:
+generateBill(items)
+Example Input:
+
+Output:
+
+
+Total:11500
+
+Scenario
+E-commerce checkout page.
+
+*/
+
+let cartItems = 
+[{ name:"Mouse", price:500 },
+{ name:"Keyboard", price:1000 },
+{ name:"Monitor", price:10000 }
+]
+
+function generateBill(items){
+    let total = 0;
+    for(let val of items){
+        total += val.price;   // 0 + undefined = NaN
+    }
+    return total;
+}
+
+console.log(generateBill(cartItems));
